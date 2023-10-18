b2a1428f14ae5d9c659efa9c30c7c07e8fa79a7d
Sinha899 <143953851+Sinha899@users.noreply.github.com>
2023-10-18 17:32:53 -0500

    Add files via upload

---
+++ storie.js
@@ -0,0 +1,54 @@
+function li(){
+    a=Number(prompt("Is this your favorite movie?(put 1 for yes and 2 for no)"));
+    if(a==1){
+        alert("This is my 6th favorite ");
+    }
+    if(a==2){
+        alert(" I agree with you. This is my 6th favorite ");
+    }
+}
+function ji(){
+    a=Number(prompt("Is this your favorite movie?(put 1 for yes and 2 for no)"));
+    if(a==1){
+        alert("This is my 5th favorite. ");
+    }
+    if(a==2){
+        alert(" I agree with you. This is my 5th favorite.");
+    }
+}
+function hi(){
+    a=Number(prompt("Is this your favorite movie?(put 1 for yes and 2 for no)"));
+    if(a==1){
+        alert("This is my favorite. ");
+    }
+    if(a==2){
+        alert(" I disagree with you. This is my favorite.");
+    }
+}
+function pi(){
+    a=Number(prompt("Is this your favorite movie?(put 1 for yes and 2 for no)"));
+    if(a==1){
+        alert("This is my 4th favorite. ");
+    }
+    if(a==2){
+        alert(" I agree with you. This is my 4th favorite.");
+    }
+}
+function ri(){
+    a=Number(prompt("Is this your favorite movie?(put 1 for yes and 2 for no)"));
+    if(a==1){
+        alert("This is my 2th favorite. ");
+    }
+    if(a==2){
+        alert(" I agree with you. This is my 2th favorite.");
+    }
+}
+function qi(){
+    a=Number(prompt("Is this your favorite movie?(put 1 for yes and 2 for no)"));
+    if(a==1){
+        alert("This is my 3th favorite. ");
+    }
+    if(a==2){
+        alert(" I agree with you. This is my 3th favorite.");
+    }
+}
